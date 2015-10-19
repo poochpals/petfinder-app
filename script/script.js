ca34e2a192f9ad05aedc00c23305a95b0a1e528a
Samantha Cross <samm.cross@gmail.com>
2015-10-19 17:46:07 -0400

    Pet finder app initial

---
+++ script/script.js
@@ -0,0 +1,39 @@
+var petFinder = {};
+
+petFinder.apiKey = 'abef3b0336a3c5097e1138a4f5c127df1';
+petFinder.apiSecret = '7e8338ee0e78c9548e81e40f0eef7191';
+
+
+petFinder.getData = function(variable, city) {
+	$.ajax({
+		url: 'http://api.petfinder.com/pet.find?key=bef3b0336a3c5097e1138a4f5c127df1&location=Toronto,ontario&format=json&animal=dog',
+
+
+
+		// 'http://api.petfinder.com/pet.find?key=bef3b0336a3c5097e1138a4f5c127df1&location=Toronto,ontario&format=json&animal=dog',
+		dataType: 'jsonp',
+		method: 'GET', 
+		data: {
+			variable: "find",
+			key: petFinder.apiKey,
+			location: city, 
+			format: 'json',
+			animal: "dog" 
+		}
+	}).then(function(res){
+		console.log(res);
+	});
+};
+
+
+
+
+
+//Document ready. Runs everything once it's ready. 
+$(document).ready(function(){
+  petFinder.getData();
+  petFinder.getData("shelter", "Toronto, Ontario");
+  // petFinder.getData("M6P1N9");
+});
+
+
