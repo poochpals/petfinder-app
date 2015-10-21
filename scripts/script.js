c86a1e43f4764afc60be18d173f3a1267a58ae98
Lester Carreon <lester.carreon@gmail.com>
2015-10-21 18:19:02 -0400

    Added flickicty script and styles

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ petFinder.apiKey = 'abef3b0336a3c5097e1138a4f5c127df1';
 petFinder.apiSecret = '7e8338ee0e78c9548e81e40f0eef7191';
 
 
-petFinder.getData = function(variable, city) {
+petFinder.getData = function(city) {
 	$.ajax({
 		url: 'http://api.petfinder.com/pet.find?key=bef3b0336a3c5097e1138a4f5c127df1&location=Toronto,ontario&format=json&animal=dog',
 		// 'http://api.petfinder.com/pet.find?key=bef3b0336a3c5097e1138a4f5c127df1&location=Toronto,ontario&format=json&animal=dog',
